42c14315e2aa7a01d38e462be8cd7a4a590b1afd
Antonio Cruz and Mariam Alaeddin <example@example.com>
2020-01-15 14:31:48 -0800

    add working shout-machine

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-$(DocumentType
+$(document).ready(function() {
+    $("#formOne").submit(function(event) {
+        var greetingInput = $("input#greeting").val();
+            $(".output").text(greetingInput);
+            console.log(greetingInput)
+        event.preventDefault();
+    });
+});
